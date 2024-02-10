0aa16a30b46bed640061340fe0328c43bfac8b0c
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2024-02-10 15:39:12 +0100

    owambe change

--- src/page/Resume.tsx
+++ src/page/Resume.tsx
@@ -1,7 +1,18 @@
+import { useSelector } from "react-redux";
+
 const Resume = () => {
+  const mode = useSelector((state: any) => state.mode);
   return (
-    <div id="resume" className="h-[100vh] w-full flex items-center justify-center bg-[blue] text-white">
-      Resume
+    <div
+      id="resume"
+      className={`h-[100vh] w-full flex  justify-center ${
+        !mode ? "bg-black text-white" : "bg-white text-black"
+      }`}
+    >
+      <div className="flex flex-col items-center mt-[65px]">
+        <div className="font-[Buba] text-4xl max-md:text-2xl">Resume</div>
+        <div>Currently not available</div>
+      </div>
     </div>
   );
 };

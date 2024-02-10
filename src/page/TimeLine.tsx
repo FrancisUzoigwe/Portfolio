53a77da7fd22e7daa9ecdd1a6ef53fb2476c87de
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2024-02-10 15:24:06 +0100

    changes

--- src/page/TimeLine.tsx
+++ src/page/TimeLine.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Fade } from "react-awesome-reveal";
+import { Fade } from "react-awesome-reveal";
 import { useSelector } from "react-redux";
 
 interface Experience {
@@ -53,56 +53,58 @@ const TimeLine: React.FC = () => {
   return (
     <div className="container mx-auto px-4 w-full">
       <div className="relative w-full md:w-full lg:w-full xl:w-full 2xl:w-3/4 md:mx-auto md:text-center">
-        {/* <Fade triggerOnce={true} delay={1000}> */}
         <div className="md:mx-auto  md:text-center">
           {experiences.map((experience, index) => (
-            <div
-              key={index}
-              className={`mb-8 relative  py-3 flex md:flex-row flex-col ${
-                index % 2 === 0 ? "md:items-start" : "md:items-end"
-              }`}
-              style={{ marginTop: index === 0 ? 0 : "-1rem" }}
-            >
+            <Fade triggerOnce={true} delay={1000}>
               <div
-                className={` w-full md:w-1/2 md:order-${
-                  index % 2 === 0 ? "2" : "1"
-                } ${index % 2 === 0 ? "md:pl-3" : "md:pr-3"}`}
+                key={index}
+                className={`mb-8 relative  py-3 flex md:flex-row flex-col ${
+                  index % 2 === 0 ? "md:items-start" : "md:items-end"
+                }`}
+                style={{ marginTop: index === 0 ? 0 : "-1rem" }}
               >
-                <div className="w-50px h-50px ">
-                  <div className="w-[45px] h-[45px] bg-[orange] text-white rounded-full flex items-center justify-center absolute top-5 transform -translate-x-1/2 -translate-y-1/2 left-1/2">
-                    <span className="text-[13px] font-bold">
-                      {experience.year}
-                    </span>
+                <div
+                  className={` w-full md:w-1/2 md:order-${
+                    index % 2 === 0 ? "2" : "1"
+                  } ${index % 2 === 0 ? "md:pl-3" : "md:pr-3"}`}
+                >
+                  <div className="w-50px h-50px ">
+                    <div className="w-[45px] h-[45px] bg-[orange] text-white rounded-full flex items-center justify-center absolute top-5 transform -translate-x-1/2 -translate-y-1/2 left-1/2">
+                      <span className="text-[13px] font-bold">
+                        {experience.year}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className={`w-full md:w-1/2 md:order-${
+                    index % 2 === 0 ? "1" : "2"
+                  } ${index % 2 === 0 ? "md:pr-3" : "md:pl-3"}`}
+                >
+                  <div
+                    className={`${
+                      mode ? "bg-black text-white" : "bg-white text-black"
+                    } p-4 rounded-lg shadow-md`}
+                  >
+                    <h2 className="text-lg font-bold ">{experience.title}</h2>
+                    <h2 className="text-lg font-bold ">
+                      {experience.workPlace}
+                    </h2>
+                    <li className="mb-2 w-full text-[15px] text-left">
+                      {experience.description}
+                    </li>
+                    <li className="mb-2 w-full text-[15px] text-left">
+                      {experience.description1}
+                    </li>
+                    <li className="mb-2 w-full text-[15px] text-left">
+                      {experience.description2}
+                    </li>
                   </div>
                 </div>
               </div>
-              <div
-                className={`w-full md:w-1/2 md:order-${
-                  index % 2 === 0 ? "1" : "2"
-                } ${index % 2 === 0 ? "md:pr-3" : "md:pl-3"}`}
-              >
-                <div
-                  className={`${
-                    mode ? "bg-black text-white" : "bg-white text-black"
-                  } p-4 rounded-lg shadow-md`}
-                >
-                  <h2 className="text-lg font-bold ">{experience.title}</h2>
-                  <h2 className="text-lg font-bold ">{experience.workPlace}</h2>
-                  <li className="mb-2 w-full text-[15px] text-left">
-                    {experience.description}
-                  </li>
-                  <li className="mb-2 w-full text-[15px] text-left">
-                    {experience.description1}
-                  </li>
-                  <li className="mb-2 w-full text-[15px] text-left">
-                    {experience.description2}
-                  </li>
-                </div>
-              </div>
-            </div>
+            </Fade>
           ))}
         </div>
-        {/* </Fade> */}
       </div>
     </div>
   );

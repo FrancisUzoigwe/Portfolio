a3aa4a59b219ff08c8739c1eab9103ac8b6d98eb
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2024-02-13 17:54:04 +0100

    today's changes

--- src/global/GlobalFile.tsx
+++ src/global/GlobalFile.tsx
@@ -5,12 +5,16 @@ import MainPage from "../page/MainPage";
 import Projects from "../page/Projects";
 import Resume from "../page/Resume";
 import Skills from "../page/Skills";
+import { IoIosTimer } from "react-icons/io"
 
 const GlobalFile = () => {
 
   const mode = useSelector((state: any) => state.mode)
   return (
-    <div className={`${!mode ? "bg-black text-white" : "bg-white text-black"}`}>
+    <div className={`${!mode ? "bg-black text-white" : "bg-white text-black"}`}
+    >
+      <div className="w-full flex justify-center item-center fixed mt-5">
+        <div className={` flex item-center px-7 py-3 rounded-full font-black ${mode ? "bg-[gray] text-white" : "bg-white text-black"}`}>Upgrage Occuring <div className="ml-4"><IoIosTimer className="text-2xl" /></div></div></div>
       <MainPage />
       <About />
       <Projects />
